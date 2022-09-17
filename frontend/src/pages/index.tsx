4b41cf7f24627f0e3509f7586dca47ad2923102c
Freeman Jiang <freeman.jiang.ca@gmail.com>
2022-09-17 12:39:46 -0400

    Add IRL date and location

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,14 +3,52 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { Status } from "@/components/Status";
 import { Warning } from "@/components/Warning";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useDate } from "@/hooks/useDate";
 
 const VIDEO_FEED_URL = "http://localhost:3001/video_feed";
 // const VIDEO_FEED_URL = "1.jpg";
 
 // Dimensions of the 7 in. RPI screen are 800 x 480
 const Home: NextPage = () => {
+  const [premise, setPremise] = useState("Calibrating...");
+  const [city, setCity] = useState("Calibrating...");
+  const [read, setRead] = useState(false);
+
+  useEffect(() => {
+    if (read) {
+      return;
+    }
+
+    if (typeof window !== "undefined") {
+      if (
+        typeof navigator !== "undefined" &&
+        typeof navigator.geolocation !== "undefined"
+      ) {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+          const { latitude, longitude } = position.coords;
+          const latlng = `${latitude},${longitude}`;
+          const {
+            data: { results },
+          } = await axios.get(
+            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng}&result_type=premise|locality&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}`
+          );
+          results.forEach((result: any) => {
+            if (result.types.includes("premise")) {
+              setPremise(result.address_components[0].long_name);
+            } else if (result.types.includes("locality")) {
+              setCity(result.formatted_address);
+            }
+          });
+        });
+      }
+      setRead(true);
+    }
+  }, [read]);
+
   const [showWarning, setShowWarning] = useState(false);
+  const { formattedDate } = useDate();
 
   return (
     <>
@@ -29,16 +67,16 @@ const Home: NextPage = () => {
         <Flex justifyContent="space-between">
           <Box fontSize="lg" bg="#202226" py="4" pl="6" pr="14" rounded="xl">
             <Box color="white">
-              <Heading fontSize="2xl">Waterloo</Heading>
-              <Heading fontSize="xl">Heading NW</Heading>
+              <Heading fontSize="xl">{city}</Heading>
+              <Text fontSize="lg">{premise}</Text>
             </Box>
             <Box mt="1">
               <Text>23.9 °C, Cloudy</Text>
-              <Text>7:25 AM, 2022/09/24</Text>
+              <Text>{formattedDate}</Text>
               <Text>12.94 km/h</Text>
             </Box>
           </Box>
-          <Status state="warning" onClick={() => setShowWarning(true)} />
+          <Status state="danger" onClick={() => setShowWarning(true)} />
         </Flex>
       </Box>
       {showWarning && <Warning setShowWarning={setShowWarning} />}
